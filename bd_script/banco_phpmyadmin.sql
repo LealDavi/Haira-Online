7f7e73b40716fee823a2b6c6ffffffbc371a4326
LealDavi <49492244+LealDavi@users.noreply.github.com>
2019-06-25 12:50:28 -0300

    Update banco_phpmyadmin.sql

--- bd_script/banco_phpmyadmin.sql
+++ bd_script/banco_phpmyadmin.sql
@@ -37,7 +37,7 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 INSERT INTO clube
 (id_clube, nome_clube, escudo, desafiado)
 VALUES
-("0", "Brasil", "imagens/brasil.png", ""),
+("0", "Brasil", "imagens/brasil.jpg", ""),
 ("1", "Argentina", "imagens/argentina.png", ""),
 ("2", "Chile", "imagens/chile.png", ""),
 ("3", "Uruguai", "imagens/uruguai.png", "");
@@ -47,16 +47,16 @@ INSERT INTO jogadores
 VALUES
 ("0", "0","Neymar Jr", "imagens/neymar_jr.jpg", "79", "70", "47"),
 ("1", "0","Alisson Becker", "imagens/alisson_becker.jpg", "99","56", "68"),
-("2", "0","Philippe Coutinho", "philippe_coutinho.jpg","50", "87", "69"),
-("3", "0","Everton Sousa", "imagens/everto_sousa.jpg", "60", "57", "59"),
+("2", "0","Philippe Coutinho", "imagens/philippe_coutinho.jpg","50", "87", "69"),
+("3", "0","Everton Sousa", "imagens/everton_sousa.jpg", "60", "57", "59"),
 ("4", "1","Lionel Messi", "imagens/lionel_messi.jpg", "90", "42", "88"),
 ("5", "1","Angel Di Maria", "imagens/angel_di_maria.jpg", "49","73", "93"),
 ("6", "1","Sergio Aguero", "imagens/sergio_aguero.jpg", "94", "52", "64"),
 ("7", "1","Paulo Dybala", "imagens/paulo_dybala.jpg", "95", "51", "40"),
-("8", "2","Alexi Sanchez", "imagens/alexi_sanchez.jpg", "78", "89", "67"),
+("8", "2","Alexi Sanchez", "imagens/alexis_sanchez.jpg", "78", "89", "67"),
 ("9", "2","Arturo Vidal", "imagens/arturo_vidal.jpg", "65","97", "66"),
 ("10", "2","Gabriel Arias", "imagens/gabriel_arias.jpg", "81", "76", "96"),
-("11", "2","Eduardo Vargas", "imagens/eduardo_arias.jpg", "86", "85", "100"),
+("11", "2","Eduardo Vargas", "imagens/eduardo_vargas.jpg", "86", "85", "100"),
 ("12", "3","Luis Suarez", "imagens/luis_suarez.jpg", "63", "98", "91"),
 ("13", "3","Diego Godin", "imagens/diego_godin.jpg","72", "55", "58"),
 ("14", "3","Edinson Cavani", "imagens/edinson_cavani.jpg", "43", "75", "71"),
